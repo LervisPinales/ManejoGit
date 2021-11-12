1d5b7731a75f25b30bc7bfa8c94da173d94b1b92
Lervis Pinales <lervispinales01@gmail.com>
2021-11-12 12:45:30 -0400

    Manejo de git

---
+++ Clases/Cliente.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Clases
+{
+    public class Cliente
+    {
+        private int ID;
+        private int Nombres;
+        private int Apellido;
+
+        public int Cedula
+        {
+            get => default;
+            set
+            {
+            }
+        }
+
+        public int Edad
+        {
+            get => default;
+            set
+            {
+            }
+        }
+    }
+}
